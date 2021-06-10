2b44f06c9203b27bc545ba8dfab99c29f3d9fdc5
Siyeon-dev <siyeon.dev@gmail.com>
2021-06-10 23:46:40 +0900

    useInput part One

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const useInput = (initialValue) => {
+	const [value, setValue] = useState(initialValue);
+	// 다른 func에서 이벤트를 처리할 수 있다.
+	const onChange = (event) => {
+		console.log(event.target);
+	};
+
+	return { value, onChange };
+};
+
+const App = () => {
+	const name = useInput("Mr. ");
+
+	return (
+		<div>
+			<h1>Hello</h1>
+			<input placeholder='Name' {...name} />
+		</div>
+	);
+};
 
 export default App;
